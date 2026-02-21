c692076aa1fe9b0542161f9b80382b51a3c4c8fc
walidBaydoun <baydounwalid4@gmail.com>
2026-02-21 11:13:08 +0200

    final code

--- 350/tutorial_hw1/students.py
+++ 350/tutorial_hw1/students.py
@@ -1,1 +1,35 @@
-print("hello world")
+import sqlite3
+
+db = sqlite3.connect("school.db")
+db.execute("PRAGMA foreign_keys=on") 
+cursor = db.cursor()
+
+cursor.execute('''CREATE TABLE student (
+    studentID int PRIMARY KEY, 
+    stdname text, 
+    stdage int)''')
+
+cursor.execute('''CREATE TABLE registered_courses (
+    studentID int, 
+    courseID text,
+    FOREIGN KEY(studentID) REFERENCES student(studentID))''')
+
+cursor.execute('''CREATE TABLE grades (
+    studentID int, 
+    courseID text, 
+    grade real,
+    FOREIGN KEY(studentID) REFERENCES student(studentID))''')
+
+db.commit()
+cursor.execute('''SELECT studentID, courseID, MAX(grade) 
+                  FROM grades 
+                  GROUP BY studentID''')
+max_grades = cursor.fetchall() 
+print("Max Grades:", max_grades)
+cursor.execute('''SELECT studentID, AVG(grade) 
+                  FROM grades 
+                  GROUP BY studentID''')
+avg_grades = cursor.fetchall()
+print("Average Grades:", avg_grades)
+
+db.close()
